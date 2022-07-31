ad5783a4fd373ff02a656b0f7173dfd9768c7a61
cydeosinem <sinemtaswork@gmail.com>
2022-07-31 21:33:44 +0300

    I finished my job

---
+++ src/FeatureGroup08/Test01.java
@@ -0,0 +1,7 @@
+package FeatureGroup08;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("Hello from Group08");
+    }
+}
---
+++ src/FeatureSinem/testSinem.java
@@ -0,0 +1,4 @@
+package FeatureSinem;
+
+public class testSinem {
+}
