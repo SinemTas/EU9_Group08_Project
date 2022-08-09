c12193a8e7148c25e729ed7b4d159d57f7eb7501
cydeosinem <sinemtaswork@gmail.com>
2022-08-09 21:30:22 +0300

    checked repository

--- src/FeatureGroup08/Test01.java
+++ src/FeatureGroup08/Test01.java
@@ -3,5 +3,6 @@ package FeatureGroup08;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("Hello from Group08");
+        System.out.println("created git repository");
     }
 }
