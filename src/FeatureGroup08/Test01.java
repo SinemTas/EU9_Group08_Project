c33e1e87423296156b017886e3be6fefbadd5102
cydeosinem <sinemtaswork@gmail.com>
2022-08-12 00:30:52 +0300

    commited this files before pull

--- src/FeatureGroup08/Test01.java
+++ src/FeatureGroup08/Test01.java
@@ -4,5 +4,6 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("Hello from Group08");
         System.out.println("created git repository");
+        System.out.println("commiting before pull");
     }
 }
